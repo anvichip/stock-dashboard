598a0fac98eacedb7eab6f4ad898f055b00ac31e
PoyBoy <parvsharma0104@gmail.com>
2024-01-08 03:16:53 +0530

    Add ARIMA prediction

--- get_prediction.py
+++ get_prediction.py
@@ -1,15 +1,17 @@
 from graphs import *
+from arima_runner import *
 
 
 def generate_stock_page(df):
-    # generate_vwap_graph(df)
-    # generate_macd_graph(df)
-    # generate_supertrend_graph(df)
-    # generate_rsi_graph(df)
+    generate_vwap_graph(df)
+    generate_macd_graph(df)
+    generate_supertrend_graph(df)
+    generate_rsi_graph(df)
     print(type(df))
-    result = get_prediction(df)
+    # result = get_prediction(df)
+    result = run_this(df)
     return result
 
 
-def get_prediction(df):
-    return 0
+# def get_prediction(df):
+#     return 0
